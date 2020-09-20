fcbda867b99e703afd21bd06df71d1aebe323e64
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2020-09-20 20:21:40 -0300

    So, robotto sprites, interface and some animations that i forgot to put in the correct folder uwu

---
+++ MiniZJam copy/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public void SetVolume(float volume) {
+
+    }
+    public void LoadGame() {
+
+    }
+    public void ExitGame() {
+        Application.Quit();
+        Debug.Log("ah salistes wey");
+    }
+
+}
